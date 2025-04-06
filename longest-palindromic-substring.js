7185861ca4ceff39408dc24b2ec6cb10ff6963db
cocobongo <davce21_89@hotmail.com>
2025-04-05 19:43:39 -0500

    expand method

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -66,3 +66,38 @@ console.log(longestPalindrome('abcbaoabcba'));
 
 
 
+
+// expand method, also sc: O(n^2)
+
+var longestPalindrome2 = function(s){
+
+    let max = '';
+
+    function checkAtIndex2(left, right){
+
+        while(left >= 0 && right < s.length && s[left] === s[right]){
+            left--;
+            right++;
+        }
+        console.log(left + 1, right, s.slice(left + 1, right));
+        return s.slice(left + 1, right);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let odd = checkAtIndex2(i, i);
+        let even = checkAtIndex2(i, i + 1);
+
+        if(odd.length >  max.length){
+            max = odd;
+        }
+
+        if(even.length >  max.length){
+            max = even;
+        }
+    }
+    
+    return max;
+};
+
+
+console.log(longestPalindrome2('bb'));
