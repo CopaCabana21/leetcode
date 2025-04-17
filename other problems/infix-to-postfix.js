203fa516aa3790d79688bb34e9526bed6be22dca
cocobongo <davce21_89@hotmail.com>
2025-04-17 17:42:46 -0500

    improved infix-to-postfix

--- other problems/infix-to-postfix.js
+++ other problems/infix-to-postfix.js
@@ -5,14 +5,14 @@
 
 function infixToPostfix(str){
 
-    const stack = ['@'];
+    const stack = [];
     let postfix = '';
 
-    const priority = {'^':1, '*':2, '/':2, '+':4, '-':4, '@':6}
+    const priority = {'^':3, '*':2, '/':2, '+':1, '-':1}
 
     for(let i = 0; i < str.length; i++){
-        // console.log(i, postfix);
         // console.log(i, str[i], priority[str[i]], stack, postfix);
+        
 
         if(str[i] === '('){
             stack.push('(');
@@ -21,33 +21,28 @@ function infixToPostfix(str){
 
         if(str[i] == ')'){
             // console.log(i, str[i], priority[str[i]], stack, postfix);
-
             while(stack[stack.length - 1] !== '('){
-                postfix = postfix + stack[stack.length - 1];
-                stack.pop();
+                postfix = postfix + stack.pop();
+                // stack.pop();
             }
-            
+
             stack.pop();
             continue;
         }
 
         if(priority[str[i]]){
             // check priority
-            if(priority[str[i]] >= priority[stack[stack.length - 1]]){
-
-                // console.log(i, str[i], priority[str[i]], stack, postfix);
-
-                while(priority[str[i]] >= priority[stack[stack.length - 1]]){
-                    postfix = postfix + stack[stack.length - 1];
-                    stack.pop();
-                }
-
-                stack.push(str[i]);
-
-            }else{
+            if(priority[str[i]] > priority[stack[stack.length - 1]] || stack.length === 0 || stack[stack.length - 1] === '('){
 
                 stack.push(str[i])
 
+            }else{
+
+                while(priority[str[i]] <= priority[stack[stack.length - 1]]){
+                    postfix = postfix + stack.pop();
+                }
+
+                stack.push(str[i]);
             }
 
         }else{
@@ -57,14 +52,14 @@ function infixToPostfix(str){
 
     }
 
-    // add remainder symbols
-    while(stack.length !== 1){
+    // add remaining symbols
+    while(stack.length){
         postfix = postfix + stack.pop();
     }
 
     return postfix
 }
 
-// console.log(infixToPostfix("a*b+c^d/e^f"));
-console.log(infixToPostfix("(p+q)*(m-n)"));
+console.log(infixToPostfix("a*b+c^d/e^f"));
+// console.log(infixToPostfix("(p+q)*(m-n)"));
 // console.log(infixToPostfix("a+b*(c^d-e)^(f+g*h)-i"));
