d5093bd89d9150b7194f788d6351b1b703a94316
cocobongo <davce21_89@hotmail.com>
2025-06-09 13:24:22 -0500

    redo k group

--- reverse-nodes-in-k-group.js
+++ reverse-nodes-in-k-group.js
@@ -1,63 +1,58 @@
-
 function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
 }
 
-
-import { arrayToLinkedList } from "./utility/linked list.js";
+import { arrayToLinkedList, linkedListToArray } from "./utility/linked list.js";
 
 /**
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function(head, k) {
+var reverseKGroup = function (head, k) {
+  let count = 1;
 
-    let count = 1;
+  let dummy = new ListNode(-1);
+  dummy.next = head;
 
-    let dummy = new ListNode(-1);
-    dummy.next = head;
-    
-    let last = dummy, curr = head, forth;
+  let last = dummy,
+    curr = head,
+    forth;
 
-    while(curr){
-        forth = curr.next;
-        
-        if(count % k === 0){
-            curr.next = null;
-            let tail = last.next;
-            last.next = reverseLL(last.next);
-            last = tail;
-            last.next = forth;
-        }
+  while (curr) {
+    forth = curr.next;
 
-        count++;
-        curr = forth;
+    if (count % k === 0) {
+      curr.next = null;
+      let tail = last.next;
+      last.next = reverseLL(last.next);
+      last = tail;
+      last.next = forth;
     }
 
+    count++;
+    curr = forth;
+  }
 
-    return dummy.next;
-
+  return dummy.next;
 };
 
+function reverseLL(head) {
+  let curr = head,
+    back,
+    forth;
 
+  while (curr) {
+    forth = curr.next;
+    curr.next = back;
+    back = curr;
+    curr = forth;
+  }
 
+  head.next = null;
 
-function reverseLL(head){
-
-    let curr = head, back, forth;
-
-    while(curr){
-        forth = curr.next;
-        curr.next = back;
-        back = curr;
-        curr = forth;
-    }
-
-    head.next = null;
-
-    return back;
+  return back;
 }
 
 // tc: O(2n)
@@ -68,60 +63,117 @@ function reverseLL(head){
 
 // -------------------------------------------------------------------
 
+var reverseKGroup2 = function (head, k) {
+  let dummy = new ListNode(-1),
+    curr = head;
+  dummy.next = head;
 
-var reverseKGroup = function(head, k) {
+  let last = dummy,
+    back = null,
+    forth;
+  let count = 0;
 
-    let dummy = new ListNode(-1), curr = head;
-    dummy.next = head;
+  while (curr) {
+    // console.log('count: %d\n node:\n %o', count, curr);
 
-    let last = dummy, back = null, forth;
-    let count = 0;
+    // reverse nodes
+    forth = curr.next;
+    curr.next = back;
+    back = curr;
+    curr = forth;
 
-    while(curr){
-        // console.log('count: %d\n node:\n %o', count, curr);
+    // if(count === 1) console.log('count: %d\n node:\n %o', count, back);
 
-        // reverse nodes
-        forth = curr.next;
-        curr.next = back;
-        back = curr;
-        curr = forth;
+    if ((count + 1) % k === 0) {
+      let temp = last.next;
+      last.next = back;
 
-        // if(count === 1) console.log('count: %d\n node:\n %o', count, back); 
-        
-        if((count + 1) % k === 0){
-
-            let temp = last.next;
-            last.next = back;
-
-            temp.next = curr;
-            last = temp;
-            // console.log('count: %d\n node:\n %o', count, getKnode(curr, k));
-            // check if there's still a group to process.
-            // If not then get out of loop
-            if(getKnode(curr, k) === -1){
-                break;
-            };
-        }
-
-        count++;
+      temp.next = curr;
+      last = temp;
+      // console.log('count: %d\n node:\n %o', count, getKnode(curr, k));
+      // check if there's still a group to process.
+      // If not then get out of loop
+      if (getKnode(curr, k) === -1) {
+        break;
+      }
     }
 
-    function getKnode(head, k){
+    count++;
+  }
 
-        let curr = head;
+  function getKnode(head, k) {
+    let curr = head;
 
-        while(curr && k !== 1){
-            curr = curr.next;
-            k--;
-        }
-        
-        return (curr === null)? -1 : curr;
+    while (curr && k !== 1) {
+      curr = curr.next;
+      k--;
     }
 
-    // console.log('count: %d\n node:\n %o', count, dummy.next);
+    return curr === null ? -1 : curr;
+  }
 
-    return dummy.next;
-}
+  // console.log('count: %d\n node:\n %o', count, dummy.next);
 
+  return dummy.next;
+};
 
-JSON.stringify(reverseKGroupAgain(arrayToLinkedList([1,2,3,4,5,6,7,8]), 5));
+// JSON.stringify(
+//   reverseKGroup2(arrayToLinkedList([1, 2, 3, 4, 5, 6, 7, 8]), 5)
+// );
+
+// -----------------------------------------------------------------------------
+// again
+
+var reverseKGroup3 = function (head, k) {
+
+  const lookForTail = (head) => {
+    let count = 1;
+    let curr = head;
+    while (count % k && curr) {
+      curr = curr.next;
+      count++;
+    }
+
+    return curr
+  }
+
+  let tail = lookForTail(head);
+  if (!tail) return head;
+
+  let dummy = new ListNode(-1, head);
+  let curr = head, prev = null, next;
+  head = tail;
+  let temp;
+
+  while (curr) {
+    console.log(curr.val);
+
+    if (curr === tail) {
+      console.log('H');
+      
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+
+      temp = dummy.next;
+      dummy.next.next = next;
+      dummy.next = prev;
+      dummy = temp;
+
+      tail = lookForTail(curr);
+      if (!tail) break;
+      continue;
+    }
+
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+
+  }
+  return head;
+};
+
+let reversedLL = reverseKGroup3(arrayToLinkedList([1,2,3,4,5]), 2)
+console.log(linkedListToArray(reversedLL));
