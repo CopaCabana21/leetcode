a23743a1079a2c418a4a7ca026deb660b892dbbb
cocobongo <davce21_89@hotmail.com>
2025-06-12 16:51:20 -0500

    add-two-numbers: redo

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -5,76 +5,72 @@
  */
 
 function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
 }
 
 
-import { arrayToLinkedList } from "./utility/linked list.js";
+import { arrToll, llToArr } from "./utility/linked list.js";
 
-var addTwoNumbers = function(l1, l2) {
+var addTwoNumbers = function (l1, l2) {
 
-    let sum, carry = 0;
-    let dummy = new ListNode(-1);
-    let current = dummy;
+  let sum, carry = 0;
+  let dummy = new ListNode(-1);
+  let current = dummy;
 
-    
-    while(l1 || l2 || carry){
 
-        // resolve when on linked list reaches the end
-        const val1 = l1? l1.val : 0;
-        const val2 = l2? l2.val : 0;
+  while (l1 || l2 || carry) {
 
-        sum = val1 + val2 + carry;
-        current.next = new ListNode(sum%10);
-        carry = Math.floor(sum / 10);
+    // resolve when on linked list reaches the end
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
 
-        current = current.next;
-        l1 = l1? l1.next : l1;
-        l2 = l2? l2.next : l2;
-    }
+    sum = val1 + val2 + carry;
+    current.next = new ListNode(sum % 10);
+    carry = Math.floor(sum / 10);
 
-    return dummy.next;
+    current = current.next;
+    l1 = l1 ? l1.next : l1;
+    l2 = l2 ? l2.next : l2;
+  }
+
+  return dummy.next;
 };
 
-const l1 = arrayToLinkedList([9,9,9,9,9,9,9]);
-const l2 = arrayToLinkedList([9,9,9,9]);
+const l1 = arrToll([9, 9, 9, 9, 9, 9, 9]);
+const l2 = arrToll([9, 9, 9, 9]);
 
-console.log(JSON.stringify(addTwoNumbers(l1,l2)));
-// console.log(linkedListToArray(addTwoNumbers(l1,l2)));
+// console.log(JSON.stringify(addTwoNumbers(l1, l2)));
+// console.log(llToArr(addTwoNumbers(l1,l2)));
 
 
+// -------------------------------------------------------------------------
+// redo
 
-// --------------------------------------------------------------
+var addTwoNumbers3 = function (l1, l2) {
 
-var addTwoNumbersAgain = function(l1, l2) {
-    
-    let dummy = new ListNode(-1);
-    let curr = dummy, sum, carry = 0;
-    
-    while(l1 || l2 || carry){
+  let dummy = new ListNode(-1);
+  let curr = dummy;
+  let sum = 0;
+  let carry = 0;
 
-        let val1 = l1? l1.val : 0;
-        let val2 = l2? l2.val : 0;
+  while (l1 || l2 || carry) {
+    let digit = new ListNode(0);
+    let l1Val = l1 ? l1.val : 0;
+    let l2Val = l2 ? l2.val : 0;
 
-        sum = val1 + val2 + carry;
-        carry = Math.floor(sum / 10);
-        sum = sum % 10;
-        
-        let newNode = new ListNode(sum);
+    sum = l1Val + l2Val + carry;
+    digit.val = sum % 10;
+    carry = Math.floor(sum / 10);
 
-        curr.next = newNode;
-        curr = curr.next;
+    curr.next = digit;
+    curr = digit;
+    l1 = !l1 ? l1 : l1.next;
+    l2 = !l2 ? l2 : l2.next;
+  }
 
-        l1 = l1? l1.next : l1;
-        l2 = l2? l2.next : l2;
 
-    }
-
-    return dummy.next;
-
+  return head.next;
 }
 
-console.log('-'.repeat(70));
-
-addTwoNumbersAgain(arrayToLinkedList([2,4,3]),arrayToLinkedList([5,6,4]))
+console.log(llToArr(addTwoNumbers3(arrToll([2, 4, 3]), arrToll([5, 6, 4]))));
--- next-greater-element-ii.js
+++ next-greater-element-ii.js
@@ -2,28 +2,51 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var nextGreaterElements = function(nums) {
-    
-    const stack = [];
-    let len = nums.length;
-    const res = Array(len).fill(-1);
+var nextGreaterElements = function (nums) {
 
-    for (let i = 0; i < 2*nums.length; i++) {
+  const stack = [];
+  let len = nums.length;
+  const res = Array(len).fill(-1);
 
-        // console.log(i % len, nums[i % len], stack, res);
+  for (let i = 0; i < 2 * nums.length; i++) {
 
-        while(stack.length !== 0 && nums[stack[stack.length-1]] < nums[i % len]){
-            res[stack.pop()] = nums[i % len]; // O(1)
-        }
+    // console.log(i % len, nums[i % len], stack, res);
 
-        stack.push(i % len) // O(1)
+    while (stack.length !== 0 && nums[stack[stack.length - 1]] < nums[i % len]) {
+      res[stack.pop()] = nums[i % len]; // O(1)
     }
 
-    return res
+    stack.push(i % len) // O(1)
+  }
+
+  return res
 
 }
 
 // tc: O(2n) -> O(n)
 // sc: O(2n) -> O(n)
 
-console.log(nextGreaterElements([1,2,3,4,5,6,5,4,5,1,2,3]));
+console.log(nextGreaterElements([1, 2, 3, 4, 5, 6, 5, 4, 5, 1, 2, 3]));
+
+
+// ------------------------------------------------------------------------
+// redo
+
+var nextGreaterElements2 = function (nums) {
+
+  let res = Array(nums.length).fill(-1);
+  let stack = [];
+  let n = nums.length;
+
+  for (let i = 0; i < 2 * n; i++) {
+    // console.log(i % n, nums[i % n], stack);
+    while (stack.length && nums[i % n] > nums[stack[stack.length - 1]]) {
+      let top = stack.pop();
+      res[top] = nums[i % n];
+    }
+    if (i < n) stack.push(i % n);
+  }
+  return res;
+}
+
+console.log(nextGreaterElements2([1, 2, 3, 4, 5, 6, 5, 4, 5, 1, 2, 3]));
